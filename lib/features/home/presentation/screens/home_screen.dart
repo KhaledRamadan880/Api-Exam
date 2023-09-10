db6b5a3c10cdb7fc73455de1e214ea0cee071350
Khaled Ramadan <khaledramdan880@gmail.com>
2023-09-10 10:13:24 +0300

    Id Tip

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,91 +1,59 @@
+import 'package:api_exam/core/utils/app_colors.dart';
+import 'package:api_exam/core/utils/app_icons.dart';
+import 'package:api_exam/core/utils/app_strings.dart';
 import 'package:api_exam/features/home/presentation/components/image_component.dart';
+import 'package:api_exam/features/home/presentation/home_cubit/home_cubit.dart';
+import 'package:api_exam/features/home/presentation/home_cubit/home_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple[300],
-        title: const Text('Flutter Cats App toy'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.replay_outlined),
-          ),
-        ],
-      ),
-      body: const SizedBox(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        // color: Colors.blue[100],
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //! Column 1
-              Column(
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        final homeCubit = BlocProvider.of<HomeCubit>(context);
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.purple,
+            title: const Text(AppStrings.cats),
+            centerTitle: true,
+            actions: [
+              Row(
                 children: [
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                ],
-              ),
-              //! Column 2
-              Column(
-                children: [
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://t4.ftcdn.net/jpg/00/77/51/81/360_F_77518136_F88I0v3R2mZsKEgxxXMc4iqXlOjK8OLE.jpg",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                ],
-              ),
-              //! Column 3
-              Column(
-                children: [
-                  ImageComponent(
-                    image:
-                        "https://t4.ftcdn.net/jpg/00/77/51/81/360_F_77518136_F88I0v3R2mZsKEgxxXMc4iqXlOjK8OLE.jpg",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://media.istockphoto.com/id/174846251/photo/portrait-of-a-cat.jpg?s=612x612&w=0&k=20&c=36RKBn1xvkjct4_7BuxFQzUwxb-aUraZFsWzFFUIwqE=",
-                  ),
-                  ImageComponent(
-                    image:
-                        "https://t4.ftcdn.net/jpg/00/77/51/81/360_F_77518136_F88I0v3R2mZsKEgxxXMc4iqXlOjK8OLE.jpg",
+                  const Text(AppStrings.refresh),
+                  IconButton(
+                    onPressed: () {
+                      homeCubit.getAllCats();
+                    },
+                    icon: AppIcons.refresh,
                   ),
                 ],
               ),
             ],
           ),
-        ),
-      ),
+          body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: MasonryGridView.count(
+              crossAxisCount: 3,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Tooltip(
+                  message: state is GetCatsSuccessState
+                  ? homeCubit.cats[index]['id']
+                  : '',
+                  child: ImageComponent(
+                    image: index,
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
-
-
