b1e817a5382c381ce8bcad6c20a66d1a9bdc4145
Khaled Ramadan <khaledramdan880@gmail.com>
2023-09-10 09:53:02 +0300

    Utils

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color purple = Colors.purpleAccent;
+  static const Color black12 = Colors.black12;
+  static const Color grey = Colors.grey;
+  static const Color white = Colors.white;
+}
---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const Widget refresh = Icon(Icons.replay);
+}
